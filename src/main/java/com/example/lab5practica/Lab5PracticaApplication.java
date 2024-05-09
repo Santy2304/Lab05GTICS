463e206c481b99d193a727471b09303de3aef0a1
Santy2304 <a20210751@pucp.edu.pe>
2024-05-09 09:33:08 -0500

    inicio de proyecto

---
+++ src/main/java/com/example/lab5practica/Lab5PracticaApplication.java
@@ -0,0 +1,13 @@
+package com.example.lab5practica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab5PracticaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab5PracticaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lab5practica/Lab5PracticaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab5practica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab5PracticaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
